6875d29c9ee7a6a7920deb241c64a347c201195e
Sergey <mcheguevara2@gmail.com>
2017-01-24 17:48:05 +0300

    fix

--- swipeTrack.js
+++ swipeTrack.js
@@ -1,3 +1,15 @@
+
+//closest polyfill
+(function(ELEMENT) {
+    ELEMENT.matches = ELEMENT.matches || ELEMENT.mozMatchesSelector || ELEMENT.msMatchesSelector || ELEMENT.oMatchesSelector || ELEMENT.webkitMatchesSelector;
+    ELEMENT.closest = ELEMENT.closest || function closest(selector) {
+        if (!this) return null;
+        if (this.matches(selector)) return this;
+        if (!this.parentElement) {return null}
+        else return this.parentElement.closest(selector)
+      };
+}(Element.prototype));
+
 class SwipeTrack {
   constructor(options = {}) {
     this.__debug               = options.debug || false                 // bool
